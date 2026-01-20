5e8772295687523f7e1e58daa23f516d3bf3ec2c
tarunsingh-eng <133125226+tarunsingh-eng@users.noreply.github.com>
2026-01-20 11:28:08 +0530

    Add files via upload

--- src/test/java/SuccessfulLoginTest.java
+++ src/test/java/SuccessfulLoginTest.java
@@ -4,20 +4,26 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SuccessfulLoginTest{
     @Test
     public void LoginTest(){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+            );
             WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver();
+            WebDriver driver = new ChromeDriver(options);
             driver.get("https://rahulshettyacademy.com/locatorspractice/");
             driver.findElement(By.cssSelector("input[placeholder='Username']")).sendKeys("Rahul");
             driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-            driver.findElement(By.className("submit")).click();
-
-             
+            driver.findElement(By.className("submit")).click();    
     }
 }

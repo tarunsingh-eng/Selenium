c7c1a6028edae51f376e8285f0aae19cbd5e87c5
tarunsingh-eng <133125226+tarunsingh-eng@users.noreply.github.com>
2026-02-09 13:00:10 +0530

    Add files via upload

--- src/test/java/tests/WebsiteBasicTest.java
+++ src/test/java/tests/WebsiteBasicTest.java
@@ -25,7 +25,7 @@ public class WebsiteBasicTest {
         driver.get("https://tarunsingh.co.in/");
         
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
 
         wait.until(ExpectedConditions.titleContains("Tarun Singh"));
 

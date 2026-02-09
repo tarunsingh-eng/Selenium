ff0c221b29ed3b3ab384508128418b0b56b256bf
tarunsingh-eng <133125226+tarunsingh-eng@users.noreply.github.com>
2026-02-09 12:49:17 +0530

    Add files via upload

---
+++ src/test/java/tests/WebsiteBasicTest.java
@@ -0,0 +1,41 @@
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebsiteBasicTest {
+
+    @Test
+    public void verifyTitle() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+        );
+
+        WebDriver driver = new ChromeDriver (options);
+        
+        driver.get("https://tarunsingh.co.in/");
+        
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h2[normalize-space()='Tarun Singh']")
+        ));
+
+        String title = driver.getTitle();
+
+        Assert.assertTrue(title.contains("Tarun Singh"));
+
+        driver.quit();
+    }
+}
